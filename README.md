51127c12a0c63ff69f886a258c47e51a891f8403
tondy67 <tondy@tondy.com>
2017-10-13 20:59:13 +0200

    add abvos node

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # abv-server
-Abv-node server using [Express 4](http://expressjs.com/).
+
+Abvos node server using [Express 4](http://expressjs.com/).
 
 # Live demos
 * [now.eona.net](https://now.eona.net/)
 * [eona.herokuapp.com](https://eona.herokuapp.com/)
 * [openshift.eona.net](http://openshift.eona.net/)
+* [abvos.cloudno.de](http://abvos.cloudno.de/)
+* [abvos.openode.io](http://abvos.openode.io/)
 
-## Tools
-* sudo npm install -g jshint
-* sudo npm install -g browserify
--- server.js
+++ server.js
@@ -3,21 +3,25 @@
  */
 "use strict";
 
-// export DEBUG=abv:* / unset DEBUG
-const debug = require('debug')('abv:srv');
+const ABV_URL = 'https://tondy67.github.io/abvos/node.html';
+
+// node --inspect server.js | Open 'about:inspect' in Chrome
+// export DEBUG=abv:*,info / unset DEBUG
+const ts = require('abv-ts')('abv:server');
+const log = console.log.bind(console);
 
 const express = require('express');
-//const abv = require('./abv-node');
+const abv = require('abv-node');
 
 const app = express();
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || abv.port;
 const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.set('port', port);
 ///
-if (app.get('port') != 8080){
-	app.all('*', ensureSecure); 
+if (app.get('port') != abv.port){
+//	app.all('*', ensureSecure); 
 }
 
 app.use(express.static(__dirname + '/public'));
@@ -25,29 +29,25 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-///
-app.get('/pgp',
+app.get('/',
 	function(req, res){
-		res.render('pages/pgp',{ title: 'PGP',url:'/pgp/'});
+		res.render('pages/index',{ title: 'Abvos',url:ABV_URL});
 });
-///
 
 app.use(function (req, res, next) {
 	res.status(404).send('404 - Not found!');
 });
 
-///
 function ensureSecure(req, res, next){
   if(req.get('x-forwarded-proto') === 'https') return next();
   res.redirect('https://' + req.hostname + req.url); 
-};
-///
+}
 
 const server = app.listen(app.get('port'), ip, 511, function(err) {
 	if (err) return console.log(err);
-	debug('Abv-node is running on http://%s:%s', ip, app.get('port'));
+	ts.info('Node.js: ' + process.version);
+	log('Abvos node is running on http://%s:%s', ip, app.get('port'));
 });
 
-//abv.start(server);
-
+abv.node(server);
 ///
